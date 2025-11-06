a9c5b86387209bdb4dd370de7e325c1a390de3cd
matr1xjt <2543182247@qq.com>
2025-11-06 14:14:33 +0800

    hash-again

--- arceos/ulib/axstd/src/collections.rs
+++ arceos/ulib/axstd/src/collections.rs
@@ -1,27 +1,53 @@
 #[cfg(feature = "alloc")]
 use alloc::vec::Vec;
+#[cfg(feature = "alloc")]
+pub struct HashNode<K, V> {
+    key: K,
+    value: V,
+}
+
+
+pub fn hash<K: core::hash::Hash>(key: &K) -> usize {
+    use core::hash::{Hasher, BuildHasherDefault};
+    use core::hash::Hash;
+    use core::hash::SipHasher;
+
+    let mut hasher = SipHasher::new();
+    key.hash(&mut hasher);
+    hasher.finish() as usize
+}
 
 #[cfg(feature = "alloc")]
 pub struct HashMap<K, V> {
-    keys: Vec<K>,
-    values: Vec<V>,
+   hash : Vec<Vec<HashNode<K, V>>>,
 }
 #[cfg(feature = "alloc")]
-impl<K, V> HashMap<K, V> {
+impl<K: core::hash::Hash, V> HashMap<K, V> {
     pub fn new() -> Self {
-        Self {
-            keys: Vec::new(),
-            values: Vec::new(),
+        // 分配长度为13的HashNode数组
+        let mut hash= Vec::with_capacity(13);
+        for _ in 0..13 {
+            hash.push(Vec::new());
         }
+        Self {
+            hash,
+        }
+        
     }
 
     pub fn insert(&mut self, key: K, value: V) {
-        self.keys.push(key);
-        self.values.push(value);
+        let node = HashNode {
+            key,
+            value,
+        };
+        let index = hash(&node.key) % self.hash.capacity();
+        self.hash[index].push(node);
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (&K, &V)> {
-        self.keys.iter().zip(self.values.iter())
+        self.hash.iter().flat_map(|bucket| {
+            bucket.iter().map(|node| (&node.key, &node.value))
+        })
     }
 }
 
